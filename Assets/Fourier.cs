070fe940d1e5f2d13d33fc241c17d51f0ae6072d
Stan <stancai@dianhun.cn>
2021-01-24 17:43:45 +0800

    - implemented basic fourier transform test;

---
+++ Assets/Fourier.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fourier : MonoBehaviour
+{
+	public ComputeShader cs;
+
+	public Texture2D originalImage;
+	[Range(32, 1024)]
+	public int renderTextureSize = 1024;
+	private int CS_GRID;
+
+
+	RenderTexture rtFourierSpectrum;
+	RenderTexture rtFourierPhase;
+
+	RenderTexture rtFourierRevert;
+
+	// output
+	public MeshRenderer rendererFourierSpectrum = null;
+	public MeshRenderer rendererFourierPhase = null;
+	public MeshRenderer rendererFourierRevert = null;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		CS_GRID = renderTextureSize / 16;
+
+		rtFourierSpectrum = createRT(renderTextureSize);
+		rtFourierPhase = createRT(renderTextureSize);
+		rtFourierRevert = createRT(renderTextureSize);
+
+		initMaterial();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void doFourierTransform() {
+		Debug.Log("doFourierTransform begin...");
+
+		int kernel = cs.FindKernel("Fourier");
+
+		cs.SetInt("renderTextureSize", renderTextureSize);
+
+		cs.SetTexture(kernel, "originalImg", originalImage);
+		cs.SetTexture(kernel, "RTFourierSpectrum", rtFourierSpectrum);
+		cs.SetTexture(kernel, "RTFourierPhase", rtFourierPhase);
+
+		cs.Dispatch(0, CS_GRID, CS_GRID, 1);
+		Debug.Log("doFourierTransform done.");
+	}
+
+
+	private void initMaterial() {
+		rendererFourierSpectrum?.material.SetTexture("_MainTex", rtFourierSpectrum);
+		rendererFourierPhase?.material.SetTexture("_MainTex", rtFourierPhase);
+
+		if (rendererFourierRevert != null)
+		{
+			rendererFourierRevert.material.SetTexture("_MainTex", rtFourierRevert);
+		}
+
+
+	}
+
+	private RenderTexture createRT(int rtSize) {
+		RenderTexture ret = new RenderTexture(rtSize, rtSize, 24);
+		ret.format = RenderTextureFormat.ARGBFloat;
+		ret.enableRandomWrite = true;
+		ret.filterMode = FilterMode.Point;
+
+		ret.Create();
+		ret.DiscardContents();
+
+		return ret;
+	}
+}
